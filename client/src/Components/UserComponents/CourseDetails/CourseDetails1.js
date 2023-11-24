917b0b7200bdb95225cc35f965a45065726cbd21
Parvathi Kumar <parvathi.kumar1989@gmail.com>
2023-11-24 14:33:29 +0530

    Fix case sensitivity issues

--- client/src/Components/UserComponents/CourseDetails/CourseDetails1.js
+++ client/src/Components/UserComponents/CourseDetails/CourseDetails1.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import axios from '../../../Utils/axios';
 import './CourseDetails.css'
 
 function CourseDetails() {
